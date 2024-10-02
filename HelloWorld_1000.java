56bfd9dcdb8eaaf7114abdf461c702d1031dbc07
Musfique Anam <musfique2002.personal@gmail.com>
2024-10-02 22:03:01 +0600

    rename  to HelloWorld_1000.java

--- HelloWorld_1000.java
+++ HelloWorld_1000.java
@@ -1,5 +1,5 @@
 package beecrowdBeginner;
-public class HelloWorld_1001  {
+public class HelloWorld_1000  {
     public static void main(String[] args) {
         System.out.println("Hello World!");    
     }
