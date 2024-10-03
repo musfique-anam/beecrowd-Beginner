15b7cd26b7d8e68232d61b17d99c63e63fd6baf2
Musfique Anam <musfique2002.personal@gmail.com>
2024-10-03 21:11:54 +0600

    initial

---
+++ SimpleProduct_1004.java
@@ -0,0 +1,11 @@
+package beecrowdBeginner;
+import java.util.Scanner;
+public class SimpleProduct_1004{
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int A=scan.nextInt();
+        int B=scan.nextInt();
+        int PROD=A*B;
+        System.out.println("PROD = "+PROD);
+    }
+}
