b4ec948f089b0e033bd3feb8c1b13d77f983dae9
maananto <musfique2002.personal@gmail.com>
2024-10-05 15:26:17 +0600

    initial

---
+++ problem1006.java
@@ -0,0 +1,15 @@
+package beecrowdBeginner;
+import java.io.IOException;
+import java.util.Scanner;
+public class problem1006 {
+    public static void main(String[] args) throws IOException {
+        Scanner scan=new Scanner(System.in);
+        double A,B,C,M;
+        A=scan.nextDouble();
+        B=scan.nextDouble();
+        C=scan.nextDouble();
+        M=((A*2)+(B*3)+(C*5))/10;
+        System.out.printf("MEDIA = %.1f",M);
+        System.out.println();  
+    }    
+}
