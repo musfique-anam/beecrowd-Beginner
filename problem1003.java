733198da08c1db2813263633874053271b35b97a
Musfique Anam <musfique2002.personal@gmail.com>
2024-10-10 15:25:33 +0600

    Update and rename

--- problem1003.java
+++ problem1003.java
@@ -1,6 +1,5 @@
-package beecrowdBeginner;
 import java.util.Scanner;
-public class SimpleSum_1003 {
+public class problem1003 {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int A=scan.nextInt();
