4538c828433ea89919f7e8d24d39db7b938ba029
Musfique Anam <musfique2002.personal@gmail.com>
2024-10-10 15:26:16 +0600

    Update and rename

--- problem1004.java
+++ problem1004.java
@@ -1,6 +1,5 @@
-package beecrowdBeginner;
 import java.util.Scanner;
-public class SimpleProduct_1004{
+public class problem1004 {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int A=scan.nextInt();
