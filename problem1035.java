a1d75b8b87ffef8268bcefffbe99bdfaff76a43c
maananto <musfique2002.personal@gmail.com>
2024-10-10 07:45:22 +0600

    initial

---
+++ problem1035.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class problem1035 {
+
+    public int selection(int A, int B, int C, int D) {
+        int E, F, r;
+        E = C + D;
+        F = A + B;
+        if (B > C && D > A && E > F && C >= 0 && D >= 0 && A % 2 == 0) {
+            r = -1;
+        } else
+            r = 1;
+        return r;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int A, B, C, D, result;
+        A = scan.nextInt();
+        B = scan.nextInt();
+        C = scan.nextInt();
+        D = scan.nextInt();
+
+        problem1035 m = new problem1035();
+        result = m.selection(A, B, C, D);
+
+        if (result == -1) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+
+    }
+
+}
---
+++ problem1038.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class problem1038 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int X = scan.nextInt();
+        int Y = scan.nextInt();
+        double a1 = 1, a2 = 2, a3 = 3, a4 = 4, a5 = 5, p1, p2, p3, p4, p5;
+        while (X == a1) {
+            p1 = 4.00 * Y;
+            System.out.printf("Total: R$ %.2f\n", p1);
+            break;
+        }
+        while (X == a2) {
+            p2 = 4.50 * Y;
+            System.out.printf("Total: R$ %.2f\n", p2);
+            break;
+        }
+        while (X == a3) {
+            p3 = 5.00 * Y;
+            System.out.printf("Total: R$ %.2f\n", p3);
+            break;
+        }
+        while (X == a4) {
+            p4 = 2.00 * Y;
+            System.out.printf("Total: R$ %.2f\n", p4);
+            break;
+        }
+        while (X == a5) {
+            p5 = 1.50 * Y;
+            System.out.printf("Total: R$ %.2f\n", p5);
+            break;
+        }
+
+    }
+}
