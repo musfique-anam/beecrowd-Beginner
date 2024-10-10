915041b5d1f8711bf96da23b119364e9c4a03172
Musfique Anam <musfique2002.personal@gmail.com>
2024-10-10 15:24:53 +0600

    Update and rename

--- problem1002.java
+++ problem1002.java
@@ -1,6 +1,5 @@
-package beecrowdBeginner;
 import java.util.Scanner;
-public class AreaofaCircle_1002 {
+public class problem1002 {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         double R=scan.nextDouble();
