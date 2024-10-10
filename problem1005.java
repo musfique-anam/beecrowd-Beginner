cf8a354064fe1c6cb2905dbda7915c75e4df7b9f
Musfique Anam <musfique2002.personal@gmail.com>
2024-10-10 15:28:31 +0600

    Update and rename

--- problem1005.java
+++ problem1005.java
@@ -1,6 +1,5 @@
-package beecrowdBeginner;
 import java.util.Scanner;
-public class Average1_1005 {
+public class problem1005 {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         double A=s.nextDouble();
