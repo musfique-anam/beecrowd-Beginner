0b27f935a506929fa24bc4f82105e00d518ccc35
musfique-anam <musfique2002.personal@gmail.com>
2024-10-11 19:00:02 +0600

    Solved

---
+++ problem1073.java
@@ -0,0 +1,16 @@
+//Beginner Problem - 1073:Even Square
+import java.io.IOException;
+import java.util.Scanner;
+public class problem1073 {
+    public static void main(String[] args) throws IOException {
+        try(Scanner scan=new Scanner(System.in)){
+            int n=scan.nextInt();
+            for(int i =2;i<=n;i++){
+                if(i%2==0){
+                    int z=i*i;
+                    System.out.println(i+"^2 = "+z);
+                }
+            }
+        }
+    }
+}
