2679a16220e5d0d06c8ef782b17e1cbd64a8b044
musfique-anam <musfique2002.personal@gmail.com>
2024-10-12 10:52:56 +0600

    Unsolved

--- problem1079.java
+++ problem1079.java
@@ -1,10 +1,6 @@
 //Beginner Problem - 1079 : Weighted Averages
 /*
- * 
- * 
- * DIDNT SOLVED THIS.................................
- * 
- * 
+ * UNSOLVED
  */
 import java.io.IOException;
 import java.util.Scanner;
@@ -15,28 +11,32 @@ public class problem1079 {
             String [] N=input.split(" ");
             int size=N.length;
             System.out.println(size);
-            if (size==1){
-                double n1=Double.parseDouble(N[0]);
-                double w1=n1*2;
-                double avg1=w1/2;
-                System.out.printf("%.1f",avg1);
-            }else if(size==2){
-                double n1=Double.parseDouble(N[0]);
-                double w1=n1*2;
-                double n2=Double.parseDouble(N[1]);
-                double w2=n2*3;
-                double avg2=(w1+w2)/9;
-                System.out.printf("%.1f",avg2);
-            }else {
-                double n1=Double.parseDouble(N[0]);
-                double w1=n1*2;
-                double n2=Double.parseDouble(N[1]);
-                double w2=n2*3;
-                double n3=Double.parseDouble(N[2]);
-                double w3=n3*5;
-                double avg3=(w1+w2+w3)/9;
-                System.out.printf("%.1f",avg3);
-            } 
+            switch (size) {
+                case 1 ->                     {
+                        double n1=Double.parseDouble(N[0]);
+                        double w1=n1*2;
+                        double avg1=w1/2;
+                        System.out.printf("%.1f",avg1);
+                    }
+                case 2 ->                     {
+                        double n1=Double.parseDouble(N[0]);
+                        double w1=n1*2;
+                        double n2=Double.parseDouble(N[1]);
+                        double w2=n2*3;
+                        double avg2=(w1+w2)/9;
+                        System.out.printf("%.1f",avg2);
+                    }
+                default ->                     {
+                        double n1=Double.parseDouble(N[0]); 
+                        double w1=n1*2;
+                        double n2=Double.parseDouble(N[1]);
+                        double w2=n2*3;
+                        double n3=Double.parseDouble(N[2]);
+                        double w3=n3*5;
+                        double avg3=(w1+w2+w3)/9;
+                        System.out.printf("%.1f",avg3);
+                    }
+            }
         }       
     }
 }
