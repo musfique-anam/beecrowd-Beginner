f680f535f94d27656272b08947e1e31877bbd79e
musfique-anam <musfique2002.personal@gmail.com>
2024-10-12 10:01:18 +0600

    Solved

--- problem1078.java
+++ problem1078.java
@@ -5,11 +5,9 @@ public class problem1078 {
     public static void main(String[] args) throws IOException {
         try(Scanner scan=new Scanner(System.in)){
             int n=scan.nextInt();
-            for(int i =2;i<=n;i++){
-                if(i%2==0){
-                    int z=i*i;
-                    System.out.println(i+"^2 = "+z);
-                }
+            for(int i=1;i<11;i++){
+                int z=i*n;
+                System.out.println(i+" x "+n+" = "+z);
             }
         }
     }
