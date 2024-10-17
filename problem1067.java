1d0ab5d7395cb339bf002e0913095dee06136664
musfique-anam <musfique2002.personal@gmail.com>
2024-10-17 22:55:54 +0600

    Solved

--- problem1067.java
+++ problem1067.java
@@ -1,13 +1,11 @@
 import java.io.IOException;
 import java.util.Scanner;
 public class problem1067 {
- 
     public static void main(String[] args) throws IOException {
         try(Scanner scan=new Scanner(System.in)){
             int x=scan.nextInt();
-            int a[]=new int[x];
             
-            for(int i=1;i<a.length;i+=2){
+            for(int i=1;i<x;i+=2){
                 System.out.println(i);
             }
 
